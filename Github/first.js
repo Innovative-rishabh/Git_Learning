f5e3f2de3043d094c698a5b85f5b63598e45bd20
Rishabh Kumar <rishukumarraj8089@gmail.com>
2025-09-22 20:45:58 +0530

    made changes

--- Github/first.js
+++ Github/first.js
@@ -3,3 +3,12 @@ console.log(button);
 
 const login="Login Page Added";
 console.log(login);
+
+const payment="Integrated the Payment Gateway";
+console.log(payment);
+
+const upi="Integrated UPI";
+console.log(upi);
+
+
+
